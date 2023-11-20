bd4ec30a661cdc0380b5d55b18a875f8fa3d0493
Vishrut Gupta <107024063+VishrutGupta02@users.noreply.github.com>
2023-11-21 00:53:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # modules
 from flask import Flask, render_template, request, redirect, url_for
 from verify_email import verify_email
+from flask_mail import Mail, Message
 from cs50 import SQL
 
 app = Flask(__name__)
@@ -16,7 +17,7 @@ def email_verifier(email):
 def check_user(username):
     data = db.execute("select * from table1;")
     for i in data:
-        if i[5] == username:
+        if i["username"] == username:
             return False
     return True
 
@@ -30,17 +31,17 @@ def index():
 def savedata():
     data = db.execute("select * from table1;")
     if request.form.get("gotHelp"):
-        first_name = request.form.get("first_name").capitalize()
-        last_name = request.form.get("last_name").capitalize()
+        first_name = request.form.get("first_name").upper()
+        last_name = request.form.get("last_name").upper()
         email = request.form.get("email")
         numb = request.form.get("numb")
-        new_username = request.form.get("new_username").capitalize()
+        new_username = request.form.get("new_username").upper()
         new_pass = request.form.get("new_pass")
         confirm_pass = request.form.get("confirm_pass")
         if new_pass == confirm_pass:
             if check_user(new_username):
                 if email_verifier(email):
-                    db.execute("insert into table1 values (?, ?, ?, ?, ?, ?)", first_name, last_name, email, numb, new_username, new_pass)
+                    db.execute("insert into table1 values (?, ?, ?, ?, ?, ?, 0)", first_name, last_name, email, numb, new_username, new_pass)
                     data = db.execute("select * from table1;")
                     return render_template("loggedin.html", fname = first_name, lname = last_name, email = email, number = numb, user = new_username, passwrd = new_pass)
                 else:
@@ -50,7 +51,7 @@ def savedata():
         else:
             return_template("login.html", passmatchnot = True)
     else:
-        user = request.form.get("username")
+        user = request.form.get("username").upper()
         passwrd = request.form.get("password")
         for i in data:
             if i["username"]==user:
@@ -68,17 +69,32 @@ def amount():
         user = request.form.get("username")
         for i in data:
             if i["username"] == user:
-                db.execute
-                return render_template("loggedin.html", fname = i["fname"], lname = i["lname"], email = i["email"], number = i["numb"], user = i["username"], passwrd = i["pass"], money = i["money"], edited = True)
+                db.execute("UPDATE table1 SET money = ? WHERE username = ?", money, user)
+                return render_template("loggedin.html", fname = i["fname"], lname = i["lname"], email = i["email"], number = i["numb"], user = i["username"], passwrd = i["pass"], money = money, edited = True)
 
 @app.route("/showtable", methods = ["post"])
 def table():
-    return render_template("database.html", data = db.execute("select * from table1;")
+    return render_template("database.html", data = db.execute("select * from table1 order by fname, lname;")
 )
 
 @app.route("/contact", methods = ["post"])
 def contact_page():
     return render_template("contact.html")
+
+@app.route("/rankings", methods = ["post"])
+def rankingspage():
+    data = db.execute("select * from table1 order by money desc limit 5;")
+    n1 = data[0]["fname"] + " " + data[0]["lname"]
+    n2 = data[1]["fname"] + " " + data[1]["lname"]
+    n3 = data[2]["fname"] + " " + data[2]["lname"]
+    n4 = data[3]["fname"] + " " + data[3]["lname"]
+    n5 = data[4]["fname"] + " " + data[4]["lname"]
+    return render_template("solution.html", first = n1, second = n2, third = n3, fourth = n4, fifth = n5)
+
+@app.route("/submitform", methods = ["post"])
+def submitform():
+    return render_template("login.html")
+    ######### use flask mail, connect smtp etc
 
 
 
